8fd6b826de8e723aaadfdb817318200ec157e4fa
Arpit Kulshrestha <Arpit_Kulshrestha@external.mckinsey.com>
2016-08-11 02:16:22 +0530

    added the webhook

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ This file creates your application.
 """
 import warnings
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask,request, redirect, url_for
 
 import chatbot
 import messenger
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
 FACEBOOK_TOKEN = os.environ['FACEBOOK_TOKEN']
 bot = None
@@ -46,23 +44,6 @@ def webhook():
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
-
 
 if __name__ == '__main__':
     app.run(debug=True)
