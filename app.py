6f8d61b72742b1424724cd451519290f8a6f0834
muzammil <muzammilafsar@gmail.com>
2016-08-18 01:17:23 +0530

    'fixed'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
     Flask Documentation:     http://flask.pocoo.org/docs/
     Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
     Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-    
+
     This file creates your application.
     """
 import warnings
@@ -16,7 +16,7 @@ import messenger
 
 app = Flask(__name__)
 
-FACEBOOK_TOKEN ='EAAEAj1gffywBAOSBrW6yUKQCBzmVL2Km0bzCZAxmLZALX0PzwE82DO2YvG2I0BXPaTHMYnJWi6ZB3yZBZAxx1nRNTLqwIRHMQUSO1dcAW92IIZCnAr3SzT4naHNjbUEdem0j0fnI2oWHc1kMuAfKH0JP1MbZC2rwV3TvZBTpzomOLAZDZD'
+FACEBOOK_TOKEN ='EAACNa20bylsBAGo3VoZAxq8xvBnQcgwM0344Q990f39WE8b0OX9NuWPphqpLxVadjf9zf8ZA3k7ZBTgOLfLSYHZAZBhgPW9l1Nq9GRpKmZANKieUazSsFmF8pogN9tZCOquhlpJXHPSGrKyiKJZBokrDJGhsA068Bg1BtVg4ZAUh9cAZDZD'
 bot = None
 
 messageToSend = 'This is default. Something is not correct'
@@ -25,15 +25,22 @@ done = False
 railway_api='paxbk6508'
 
 def fetch_stnname(request):
-    context = request['context']
-    entities = request['entities']
-    userinp = first_entity_value(entities, 'location').upper()
-    code=stn_name_to_code(userinp)
-    if code=='':
-        context['stnname'] = 'Not found'
+    try:
+        context = request['context']
+        entities = request['entities']
+        userinp = first_entity_value(entities, 'location').upper()
+        code,response_code=stn_name_to_code(userinp)
+
+        if code=='':
+            context['stnname'] = 'Not found'
+            return context
+        context['stnname']=code
         return context
-    context['stnname']=code
-    return context
+    except Exception:
+        print('Exception occured')
+        context['stnname'] = 'Exception occured'
+        return context
+
 
 def first_entity_value(entities, entity):
     if entity not in entities:
@@ -52,61 +59,72 @@ def send(request, response):
     done = True
 
 def fetch_statuspnr(request):
-    context = request['context']
-    entities = request['entities']
-    pnr=str(first_entity_value(entities,'number'))
-    pnr2 = first_entity_value(entities, 'location')
-    if pnr=='None':
-        #print('no pnr from number')
-        num=(re.findall(r'\b\d+\b', str(pnr2)))
-        if len(num)>0:
-            pnr=num[0]
-        else:
-            context['missingLocation']='yes'
-            return context
+    try:
+        context = request['context']
+        entities = request['entities']
+        pnr=str(first_entity_value(entities,'number'))
+        pnr2 = first_entity_value(entities, 'location')
+        if pnr=='None':
+            #print('no pnr from number')
+            num=(re.findall(r'\b\d+\b', str(pnr2)))
+            if len(num)>0:
+                pnr=num[0]
+            else:
+                context['missingLocation']='yes'
+                return context
 
-    url = 'http://api.railwayapi.com/pnr_status/pnr/' + str(pnr) + '/apikey/' + railway_api + '/'
-    parsed_json = json.loads(requests.get(url).text)
-    error = (parsed_json['error'])
-    if str(error).lower() == 'true':
-        context['pnr_status']='Error: wrong pnr no./missing pnr'
+        url = 'http://api.railwayapi.com/pnr_status/pnr/' + str(pnr) + '/apikey/' + railway_api + '/'
+        parsed_json = json.loads(requests.get(url).text)
+        error = (parsed_json['error'])
+        if str(error).lower() == 'true':
+            context['pnr_status']='Error: wrong pnr no./missing pnr'
+            return context
+        print(parsed_json)
+        from_stn = parsed_json['from_station']
+        des_stn = parsed_json['reservation_upto']
+        date_of_jrny = parsed_json['doj']
+        total_passengers = parsed_json['total_passengers']
+        passengers = parsed_json['passengers']
+        status_list = '\nFROM ' + from_stn['name'] + ' TO ' + des_stn['name'] + ' \nDate of Journey ' + date_of_jrny + '\n'
+        for x in range(total_passengers):
+            passenger_data = passengers[total_passengers - 1]
+            status_list = status_list + ' No. ' + str(passenger_data['no']) + ' Status ' + passenger_data['current_status'] + '  ->' + passenger_data['booking_status'] + '\n'
+            total_passengers = total_passengers - 1
+        context['pnr_status']=status_list
         return context
-    print(parsed_json)
-    from_stn = parsed_json['from_station']
-    des_stn = parsed_json['reservation_upto']
-    date_of_jrny = parsed_json['doj']
-    total_passengers = parsed_json['total_passengers']
-    passengers = parsed_json['passengers']
-    status_list = '\nFROM ' + from_stn['name'] + ' TO ' + des_stn['name'] + ' \nDate of Journey ' + date_of_jrny + '\n'
-    for x in range(total_passengers):
-        passenger_data = passengers[total_passengers - 1]
-        status_list = status_list + ' No. ' + str(passenger_data['no']) + ' Status ' + passenger_data['current_status'] + '  ->' + passenger_data['booking_status'] + '\n'
-        total_passengers = total_passengers - 1
-    context['pnr_status']=status_list
-    return context
+    except Exception:
+        context['pnr_status'] = 'Exception pnr'
+        return context
 
 def fetch_statustrain(request):
-    context = request['context']
-    entities = request['entities']
-    trainno=str(first_entity_value(entities,'number'))
-    trainno2= first_entity_value(entities, 'location')
-    if trainno=='None':
-        num = (re.findall(r'\b\d+\b', str(trainno2)))
-        
-        trainno=num[0]
-    
-    url = 'http://api.railwayapi.com/live/train/'+str(trainno)+'/doj/' + str(time.strftime("%Y%m%d")) + '/apikey/' + railway_api + '/'
-    parsed_json = json.loads(requests.get(url).text)
-    print(parsed_json)
-    if str(parsed_json['response_code'])=='204':
-        context['train_status'] = 'Wrong Train no'
+    try:
+        context = request['context']
+        entities = request['entities']
+        trainno=str(first_entity_value(entities,'number'))
+        trainno2= first_entity_value(entities, 'location')
+        if trainno=='None':
+            num = (re.findall(r'\b\d+\b', str(trainno2)))
+
+            trainno=num[0]
+
+        url = 'http://api.railwayapi.com/live/train/'+str(trainno)+'/doj/' + str(time.strftime("%Y%m%d")) + '/apikey/' + railway_api + '/'
+        parsed_json = json.loads(requests.get(url).text)
+        print(parsed_json)
+        if str(parsed_json['response_code'])=='204':
+            context['train_status'] = 'Wrong Train no'
+            return context
+        if str(parsed_json['response_code']) == '510':
+            context['train_status'] = parsed_json['error']
+            return context
+        train_status = parsed_json['position']
+        context['train_status'] = train_status
         return context
-    if str(parsed_json['response_code']) == '510':
-        context['train_status'] = parsed_json['error']
+    except Exception:
+        print('Exception')
+        context['train_status'] = 'Exception train status'
         return context
-    train_status = parsed_json['position']
-    context['train_status'] = train_status
-    return context
+
+
 def fetch_stncode(request):
     context = request['context']
     entities = request['entities']
@@ -143,6 +161,7 @@ def stn_name_to_code(userinp):
     stations = parsed_json['stations']
     l_of_s = len(stations)
     searchobj = userinp
+    response_code=parsed_json['response_code']
     name_code = ''
     while l_of_s > 0:
         data = stations[l_of_s - 1]
@@ -150,7 +169,7 @@ def stn_name_to_code(userinp):
             name_code = (data['code'])
             break
         l_of_s = l_of_s - 1
-    return name_code
+    return name_code,response_code
 
 
 def fetch_train(requests):
@@ -176,39 +195,50 @@ def train_btw_stn(url):
     return train_list
 
 def fetch_cancelled(request):
-    context = request['context']
-    entities = request['entities']
-    dateinp=str(first_entity_value(entities, 'datetime'))
-    url='http://api.railwayapi.com/cancelled/date/'+dateinp[8:10]+'-'+dateinp[5:7]+'-'+dateinp[0:4]+'/apikey/'+railway_api+'/'
-    parsed_json = json.loads(requests.get(url).text)
-    trains = parsed_json['trains']
-    cancel_train = ''
-    for x in range(len(trains)):
-        data = trains[x]
-        train = data['train']
-        name = train['name']
-        num = train['number']
-        cancel_train = cancel_train + name + ' ' + num + '\n'
-    context['cancel_train']=cancel_train
-    return context
+    try:
+        context = request['context']
+        entities = request['entities']
+        dateinp=str(first_entity_value(entities, 'datetime'))
+        url='http://api.railwayapi.com/cancelled/date/'+dateinp[8:10]+'-'+dateinp[5:7]+'-'+dateinp[0:4]+'/apikey/'+railway_api+'/'
+        parsed_json = json.loads(requests.get(url).text)
+        trains = parsed_json['trains']
+        cancel_train = ''
+        for x in range(len(trains)):
+            data = trains[x]
+            train = data['train']
+            name = train['name']
+            num = train['number']
+            cancel_train = cancel_train + name + ' ' + num + '\n'
+        context['cancel_train']=cancel_train
+        return context
+    except Exception:
+        context['cancel_train']='exception cancelled'
+        return context
+
+
 def fetch_reschedule(request):
-    context = request['context']
-    entities = request['entities']
-    dateinp=str(first_entity_value(entities,'datetime'))
-    #url = 'http://api.railwayapi.com/rescheduled/date/20-07-2016/apikey/paxbk6508/'
-    url = 'http://api.railwayapi.com/rescheduled/date/' + dateinp[8:10] + '-' + dateinp[5:7] + '-' + dateinp[0:4] + '/apikey/' + railway_api + '/'
-    parsed_json = json.loads(requests.get(url).text)
-    print(parsed_json)
-    trains = parsed_json['trains']
-    reschleduled_train = ''
-    for x in range(len(trains)):
-        data = trains[x]
-        name = data['name']
-        num = data['number']
-        reschleduled_train = reschleduled_train + name + ' ' + num + '\n'
-    #print(reschleduled_train)
-    context['reschedule_train']=str(reschleduled_train)
-    return context
+    try:
+        context = request['context']
+        entities = request['entities']
+        dateinp=str(first_entity_value(entities,'datetime'))
+        #url = 'http://api.railwayapi.com/rescheduled/date/20-07-2016/apikey/paxbk6508/'
+        url = 'http://api.railwayapi.com/rescheduled/date/' + dateinp[8:10] + '-' + dateinp[5:7] + '-' + dateinp[0:4] + '/apikey/' + railway_api + '/'
+        parsed_json = json.loads(requests.get(url).text)
+        print(parsed_json)
+        trains = parsed_json['trains']
+        reschleduled_train = ''
+        for x in range(len(trains)):
+            data = trains[x]
+            name = data['name']
+            num = data['number']
+            reschleduled_train = reschleduled_train + name + ' ' + num + '\n'
+        #print(reschleduled_train)
+        context['reschedule_train']=str(reschleduled_train)
+        return context
+    except Exception:
+        print('Exception rescheduled')
+        context['reschedule_train'] = 'Exception rescheduled'
+        return context
 
 
 def say(session_id, context, msg):
@@ -234,7 +264,7 @@ actions = {
     'fetch-reschedule':fetch_reschedule,
 }
 
-client = Wit(access_token='JI2MGJMIPNKC53ROSCYCNXAOURR5STA3', actions=actions)
+client = Wit(access_token='M6QM6SA2BEQN4WRMEWN7XU2AFVAUNFDL', actions=actions)
 
 ###
 # Routing for your application.
@@ -250,7 +280,6 @@ def verify():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     payload = request.get_data()
-    print(payload)
     for sender, message in messenger.messaging_events(payload):
         print "Incoming from %s: %s" % (sender, message)
         client.run_actions(sender, message, {})
@@ -260,7 +289,6 @@ def webhook():
         else:
             print "Outgoing to %s: %s" % (sender, 'some problem interpreting')
             messenger.send_message(FACEBOOK_TOKEN, sender, 'some problem interpreting')
-    
     return "ok"
 
 @app.route('/',methods=['GET'])
@@ -273,3 +301,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
